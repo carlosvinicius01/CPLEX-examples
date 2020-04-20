0ab57bb8a4b86068b54c724aee2a8feb608a7b97
carlos <carloskic1@gmail.com>
2020-04-20 05:46:42 -0300

    a

--- JEEP/src/main.cpp
+++ JEEP/src/main.cpp
@@ -17,28 +17,22 @@ using namespace std;
 
 int main()
 {
-    srand(time(NULL));
-    int N = 4;
-    int c[N + 1][N + 1];
+    vector<vector<int>> c;
+    int n = 4;
 
-    for (int i = 0; i < N + 1; i++)
+    for (int i = 0; i <= n + 1; i++)
     {
-        for (int j = 0; j < N + 1; j++)
+        c.push_back(vector<int>());
+        for (int j = 0; j <= n + 1; j++)
         {
-            if (i == 0 || j == 0 || i == j)
-            {
-                c[i][j] = 0;
-                continue;
-            }
-            c[i][j] = rand() % 5;
+            c[i].push_back(0);
         }
     }
 
-    for (int i = 0; i < N + 1; i++)
+    for (int i = 0; i <= n + 1; i++)
     {
-        for (int j = 0; j < N + 1; j++)
+        for (int j = 0; j <= n + 1; j++)
         {
-            // printf("%d", c[i][j]);
             cout << c[i][j] << " ";
         }
         cout << "\n";
@@ -47,30 +41,45 @@ int main()
     IloEnv env;
     IloModel model(env);
 
-    IloBoolVarArray y(env, N + 1);
-    IloArray<IloBoolVarArray> x(env, N + 1);
-    IloArray<IloNumVarArray> f(env, N + 1);
-    IloArray<IloBoolVarArray> z(env, N + 1);
-    IloArray<IloBoolVarArray> h(env, N + 1);
-    
-    for (int i = 0; i < N + 1; i++)
+    IloBoolVarArray y(env, n + 2);
+    for (int i = 0; i < n + 2; i++)
     {
         model.add(y[i]);
-        model.add(y[i] == 1);
-        IloBoolVarArray v(env, N + 1);
-        x[i] = v;
-        IloBoolVarArray v2(env, N + 1);
-        z[i] = v2;
-        IloNumVarArray v1(env, N + 1, 0, IloInfinity);
-        f[i] = v1;
+    }
 
-        for (int j = 0; j < N + 1; j++)
+    IloArray<IloBoolVarArray> x(env, n + 2);
+    for (int i = 0; i < n + 2; i++)
+    {
+        x[i] = IloBoolVarArray(env, n + 2);
+        for (int j = 0; j < n + 2; j++)
         {
             if (i != j)
             {
                 model.add(x[i][j]);
-                model.add(f[i][j]);
+            }
+        }
+    }
+
+    IloArray<IloBoolVarArray> z(env, n + 2);
+    for (int i = 1; i < n + 1; i++)
+    {
+        z[i] = IloBoolVarArray(env, n + 2);
+        for (int j = 1; j < n + 1; j++)
+        {
+            if (i != j)
                 model.add(z[i][j]);
+        }
+    }
+
+    IloArray<IloIntVarArray> f(env, n + 2);
+    for (int i = 0; i < n + 2; i++)
+    {
+        f[i] = IloIntVarArray(env, n + 2, 0, IloInfinity);
+        for (int j = 0; j < n + 2; j++)
+        {
+            if (i != j)
+            {
+                model.add(f[i][j]);
             }
         }
     }
@@ -78,172 +87,120 @@ int main()
     {
         IloExpr sum(env);
 
-        // for (int i = 0; i < N + 1; i++)
-        // {
-        //     sum += y[i];
-        // }
-        // model.add(IloMaximize(env, sum));
-
-        for (int i = 1; i < N + 1; i++)
+        for (int i = 1; i < n + 1; i++)
         {
-            for (int j = 1; j < N + 1; j++)
+            sum += y[i];
+        }
+        model.add(IloMaximize(env, sum));
+
+        model.add(y[0] == 1);
+        model.add(y[n + 1] == 1);
+    }
+
+    // ARCO ENTRA
+    for (int k = 1; k < n + 2; k++)
+    {
+        IloExpr sum(env);
+        for (int i = 0; i < n + 1; i++)
+        {
+            if (i != k)
             {
-                sum += z[i][j];
+                sum += x[i][k];
             }
         }
-
-        model.add(IloMinimize(env, sum));
+        model.add(sum == y[k]);
     }
 
-    for (int i = 1; i < N + 1; i++)
+    // ARCO SAI
+    for (int k = 0; k < n + 1; k++)
     {
-        for (int j = 1; j < N + 1; j++)
+        IloExpr sum(env);
+        for (int i = 1; i < n + 2; i++)
         {
-            if (i != j)
+            if (i != k)
             {
-                IloExpr sum1(env), sum2(env);
-
-                for (int k = 0; k < N + 1; k++)
-                {
-                    if (i != k)
-                    {
-                        sum1 += f[k][i];
-                    }
-                }
-
-                for (int k = 0; k < N + 1; k++)
-                {
-                    if (j != k)
-                    {
-                        sum2 += f[j][k];
-                    }
-                }
-
-                model.add(M * z[i][j] >= sum1 - sum2);
+                sum += x[k][i];
             }
         }
+        model.add(sum == y[k]);
     }
 
-    for (int i = 0; i < N + 1; i++)
-    {
-        IloExpr sum1(env);
-        for (int j = 0; j < N + 1; j++)
-        {
-            if (i == j)
-                continue;
-            sum1 += x[i][j];
-        }
-        model.add(sum1 == y[i]);
-    }
-
-    for (int i = 0; i < N + 1; i++)
-    {
-        IloExpr sum1(env);
-        for (int j = 0; j < N + 1; j++)
-        {
-            if (i == j)
-                continue;
-            sum1 += x[j][i];
-        }
-        model.add(sum1 == y[i]);
-    }
-
-    for (int k = 1; k < N + 1; k++)
+    // OLHA O FLUXO
+    for (int k = 1; k < n + 2; k++)
     {
         IloExpr sum1(env), sum2(env);
-        for (int i = 0; i < N + 1; i++)
+        for (int i = 0; i < n + 1; i++)
         {
-            if (i == k)
-                continue;
-            sum1 += f[i][k];
+            if (i != k)
+            {
+                sum1 += f[i][k];
+            }
         }
 
-        for (int j = 0; j < N + 1; j++)
+        for (int i = 1; i < n + 2; i++)
         {
-            if (j == k)
-                continue;
-            sum2 += f[k][j];
+            if (i != k)
+            {
+                sum2 += f[k][i];
+            }
         }
-
         model.add(sum1 - sum2 == y[k]);
     }
 
-    for (int i = 0; i < N + 1; i++)
+    for (int i = 0; i < n + 1; i++)
     {
-        for (int j = 0; j < N + 1; j++)
+        for (int j = 1; j < n + 2; j++)
         {
-            model.add(f[i][j] <= (N)*x[i][j]);
-        }
-    }
-
-    for (int k = 1; k < N + 1; k++)
-    {
-        for (int l = 1; l < N + 1; l++)
-        {
-            IloExpr sum1(env), sum2(env);
-            for (int i = 0; i < N + 1; i++)
+            if (i != j)
             {
-                if (i == k)
-                    continue;
-                sum1 += f[i][k];
+                model.add(f[i][j] <= (n + 2) * x[i][j]);
             }
-
-            for (int j = 0; j < N + 1; j++)
-            {
-                if (j == l)
-                    continue;
-                sum2 += f[l][j];
-            }
-
-            // model.add(sum1 - sum2 + 1 >= c[k][l] * z[k][l]);
         }
     }
 
     IloCplex JEEP(model);
-
     JEEP.solve();
 
-    cout << "Obj Value: " << JEEP.getObjValue() << "\n";
+    cout << "Obj value: " << JEEP.getObjValue() << "\n\n";
 
-    for (int i = 0; i < N + 1; i++)
+    // for(int i = 1; i < n+1;i++)
+    // {
+    //     cout << JEEP.getValue(y[i]) << "\n";
+    // }
+
+    for (int i = 0; i < n + 1; i++)
     {
-        for (int j = 0; j < N + 1; j++)
+        for (int j = 1; j < n + 2; j++)
         {
-            if (i == j)
-                continue;
-            if (JEEP.getValue(x[i][j]) > 0.9)
+            if (i != j)
             {
-                cout << i << ", " << j << "\n";
+                if (JEEP.getValue(x[i][j]) > 0.9)
+                    cout << i << ", " << j << "\n";
             }
         }
-    }
-
-    for (int i = 0; i < N + 1; i++)
-    {
-        for (int j = 0; j < N + 1; j++)
-        {
-            if (i == j)
-            {
-                cout << 0 << " ";
-                continue;
-            }
-            cout << (JEEP.getValue(f[i][j]) > 0.1 ? JEEP.getValue(f[i][j]) : 0) << " ";
-        }
-        cout << "\n";
     }
 
     cout << "\n";
 
-    for (int i = 0; i < N + 1; i++)
+    for (int i = 0; i < n + 2; i++)
     {
-        for (int j = 0; j < N + 1; j++)
+        for (int j = 0; j < n + 2; j++)
         {
-            if (i == j)
+            if (i != j)
+            {
+                if (JEEP.getValue(f[i][j]) > 0.1)
+                {
+                    cout << JEEP.getValue(f[i][j]) << " ";
+                }
+                else
+                {
+                    cout << 0 << " ";
+                }
+            }
+            else
             {
                 cout << 0 << " ";
-                continue;
             }
-            cout << (JEEP.getValue(z[i][j]) > 0.1 ? JEEP.getValue(z[i][j]) : 0) << " ";
         }
         cout << "\n";
     }
